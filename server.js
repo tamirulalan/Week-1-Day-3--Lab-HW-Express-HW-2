2371e088e753659d45d4ccd05dedb56c27bfdd54
tamirulalan <tamirulalan@gmail.com>
2022-11-30 11:11:50 -0500

    Greeting express application created.

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
+
 const express = require ('express')
 
 let app =express()
-
-
+//Make a route '/greeting' that sends a generic greeting to the screen like "Hello, stranger".
+app.get('/greeting',(req,res)=>{
+    res.send('Hello, stranger')
+})
+app.get('/greeting/:name',(req,res)=>{
+    console.log(req.params.name)
+    res.send('Wow! Hello there,' +req.params.name)
+})
 
 app.listen(3000, ()=>{
     console.log('start listening to port number 3000 👂') 
